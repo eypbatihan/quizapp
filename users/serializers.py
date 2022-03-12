e362b3cdc985aab469ea2b770dcd51ef82d02f14
eypbatihan <eypbatihan@gmail.com>
2022-03-12 23:01:38 +0300

    tk

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,4 @@
-from pickle import TRUE
-from tkinter.ttk import Style
+
 from rest_framework import serializers,validators
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
