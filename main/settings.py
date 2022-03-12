6130c83bab142b721ca3c984e3d36f186caa97dd
eypbatihan <eypbatihan@gmail.com>
2022-03-12 22:28:45 +0300

    lastest

--- main/settings.py
+++ main/settings.py
@@ -144,13 +144,12 @@ REST_FRAMEWORK = {
 
 
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT=os.path.join(BASE_DIR,"/static/")
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
- os.path.join(BASE_DIR, 'static'),
-)
+
 
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
